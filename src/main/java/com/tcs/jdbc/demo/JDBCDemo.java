ae2f2e89c220c27c4d08759b0a7c5d1fb70057c5
hussaint93 <tcssadriwalla@gmail.com>
2021-08-16 09:32:41 +0000

    using slf4j with log4j

--- src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
+++ src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
@@ -8,13 +8,13 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import  com.tcs.jdbc.demo.JDBCDemo;
 
 public class JDBCDemo {
-	private static Logger logger = LogManager.getLogger(JDBCDemo.class);
+	private static Logger logger = LoggerFactory.getLogger(JDBCDemo.class);
 	public static void main(String[] args) {
 		String DB_URL = "jdbc:mysql://localhost/practice";
 		String DB_USER = "root";
@@ -27,7 +27,7 @@ public class JDBCDemo {
 			retrieve(statement);
 			// delete(statement);
 			List<String> reg = retrieveWithCondittion(statement, "A");
-			logger.debug(reg);
+			logger.debug(reg.toString());
 			sort(statement);
 		} catch (SQLException e) {
 			e.printStackTrace();
